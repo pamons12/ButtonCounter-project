5186637ba0b68a90af482ca0b55ff66ac7b873ab
pamons12 <47860735+pamons12@users.noreply.github.com>
2021-02-09 15:21:41 -0500

    Converted xml to linear layout

--- app/src/main/java/com/example/button_counter_amons/MainActivity.java
+++ app/src/main/java/com/example/button_counter_amons/MainActivity.java
@@ -9,20 +9,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button countButton;
-    private Button minusButton;
     private Button clearButton;
-    private int currentCount;
+    private int currentCount=0;
     private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) this.findViewById(R.id.currentCount);
-        text.setText("0");
+
+        currentCount=0;
+        countButton = (Button) this.findViewById(R.id.countButton);
+        clearButton = (Button) this.findViewById(R.id.clearButton);
+
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCount+=1;
+                text.setText(String.valueOf(currentCount));
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCount=0;
+                text.setText(String.valueOf(currentCount));
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        currentCount+=1;
     }
 }
