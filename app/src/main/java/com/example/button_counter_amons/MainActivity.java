a073938f71fd9decfb2f7e537ef12b1385d8a4f3
pamons12 <47860735+pamons12@users.noreply.github.com>
2021-02-09 16:23:57 -0500

    Basics complete, buttons work and saved state when screen turned

--- app/src/main/java/com/example/button_counter_amons/MainActivity.java
+++ app/src/main/java/com/example/button_counter_amons/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.button_counter_amons;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,38 +8,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     private Button countButton;
     private Button clearButton;
-    private int currentCount=0;
-    private TextView text;
+    private int currentCount;
+    private TextView currentCountTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currentCount=0;
-        countButton = (Button) this.findViewById(R.id.countButton);
-        clearButton = (Button) this.findViewById(R.id.clearButton);
+        currentCountTextView = (TextView) findViewById(R.id.currentCountID);
 
-        countButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentCount+=1;
-                text.setText(String.valueOf(currentCount));
-            }
-        });
-
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentCount=0;
-                text.setText(String.valueOf(currentCount));
-            }
-        });
+        if (savedInstanceState != null){
+            currentCount = savedInstanceState.getInt("savedCount");
+            currentCountTextView.setText(String.valueOf(currentCount));
+        }else{
+            currentCount=0;
+        }
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onSaveInstanceState(Bundle saveState) {
+        super.onSaveInstanceState(saveState);
+        saveState.putInt("savedCount",currentCount);
+    }
+
+    public void countClicked(View view) {
+        currentCount+=1;
+        currentCountTextView.setText(String.valueOf(currentCount));
+    }
+
+    public void clearClicked(View view) {
+        currentCount=0;
+        currentCountTextView.setText(String.valueOf(currentCount));
     }
 }
